161b64586e682a7f9a17e16184df673b1074d739
dkpark10 <dkpark10@naver.com>
2024-02-11 02:06:28 +0900

    feat: zod 스키마 검증

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 // import Cell from "./components/cell";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.scss";
 import { clsx } from "clsx";
+import { z } from "zod";
+
+const LOCAL_STORAGE_KEY = "MANDATRA";
+const MandatraValueSchema = z.array(z.array(z.string()));
+
+type MandatraValues = z.infer<typeof MandatraValueSchema>;
 
 export default function Mandatra() {
-  const [values, setValues] = useState(
+  const [values, setValues] = useState<MandatraValues>(
     Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => ""))
   );
 
@@ -15,18 +21,26 @@ export default function Mandatra() {
     const { value } = e.target;
 
     setValues((prev) => {
-      /** @desc 중앙 서브 타이틀 입력 칸이라면 */
-      if (isCenterArea && col !== 4) {
-        prev[col][row] = value;
       /** @desc 서브 타이틀 입력 칸이라면 */
-      } else if (col === 4) {
+      if ((isCenterArea && col !== 4) || (col === 4)) {
         prev[col][row] = value;
       }
 
       prev[row][col] = value;
       return [...prev];
-    })
+    });
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values));
   }
+
+  useEffect(() => {
+    const val = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (!val) return;
+    const result = MandatraValueSchema.safeParse(JSON.parse(val));
+    if (result.success) {
+      setValues(result.data);
+    }
+  }, []);
 
   return (
     <main className={styles["main-container"]}>
