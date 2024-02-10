e8c6915b59b2a9ae8e39bc31d34dab7bd367c7a7
dkpark10 <dkpark10@naver.com>
2024-02-11 02:32:11 +0900

    feat: html2canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 // import Cell from "./components/cell";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./page.module.scss";
 import { clsx } from "clsx";
 import { z } from "zod";
+import html2canvas from "html2canvas";
 
 const LOCAL_STORAGE_KEY = "MANDATRA";
 const MandatraValueSchema = z.array(z.array(z.string()));
@@ -12,26 +13,34 @@ const MandatraValueSchema = z.array(z.array(z.string()));
 type MandatraValues = z.infer<typeof MandatraValueSchema>;
 
 export default function Mandatra() {
+  const mainRef = useRef<HTMLDivElement>(null);
   const [values, setValues] = useState<MandatraValues>(
     Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => ""))
   );
 
-  const onChange = (row: number, col: number) => (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const isCenterArea = row === 4;
-    const { value } = e.target;
+  const onImageSaveClick = async () => {
+    if (!mainRef.current) return;
+    const canvas = await html2canvas(mainRef.current);
+  };
 
-    setValues((prev) => {
-      /** @desc 서브 타이틀 입력 칸이라면 */
-      if ((isCenterArea && col !== 4) || (col === 4)) {
-        prev[col][row] = value;
-      }
+  const onChange =
+    (row: number, col: number) =>
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const isCenterArea = row === 4;
+      const { value } = e.target;
 
-      prev[row][col] = value;
-      return [...prev];
-    });
+      setValues((prev) => {
+        /** @desc 서브 타이틀 입력 칸이라면 */
+        if ((isCenterArea && col !== 4) || col === 4) {
+          prev[col][row] = value;
+        }
 
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values));
-  }
+        prev[row][col] = value;
+        return [...prev];
+      });
+
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values));
+    };
 
   useEffect(() => {
     const val = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -43,23 +52,31 @@ export default function Mandatra() {
   }, []);
 
   return (
-    <main className={styles["main-container"]}>
-      {values.map((rowValue, row) => {
-        const isCenterArea = row === 4;
-        const centerStyle = isCenterArea && styles["center"];
+    <div className={styles["container"]}>
+      <main ref={mainRef} className={styles["main-container"]}>
+        {values.map((rowValue, row) => {
+          const isCenterArea = row === 4;
+          const centerStyle = isCenterArea && styles["center"];
 
-        return (
-          <section key={row} className={clsx(styles["cell-container"], centerStyle)}>
-            {rowValue.map((colValue, col) => {
-              return (
-                <div key={`${row}-${col}`} className={styles["cell"]}>
-                  <textarea value={colValue} onChange={onChange(row, col)} />
-                </div>
-              );
-            })}
-          </section>
-        );
-      })}
-    </main>
+          return (
+            <section
+              key={row}
+              className={clsx(styles["cell-container"], centerStyle)}
+            >
+              {rowValue.map((colValue, col) => {
+                return (
+                  <div key={`${row}-${col}`} className={styles["cell"]}>
+                    <textarea value={colValue} onChange={onChange(row, col)} />
+                  </div>
+                );
+              })}
+            </section>
+          );
+        })}
+      </main>
+      <button className={styles["save-btn"]} onClick={onImageSaveClick}>
+        이미지 저장하기
+      </button>
+    </div>
   );
 }
