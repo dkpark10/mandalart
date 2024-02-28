02563a94da7a30481b5b16e47a2c7f33baed53d4
dkpark10 <dkpark10@naver.com>
2024-02-28 23:17:22 +0900

    feat: 라인 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,24 @@ import { ImArrowDownLeft } from "react-icons/im";
 const LOCAL_STORAGE_KEY = "MANDATRA";
 const MandatraValueSchema = z.array(z.array(z.string()));
 
-type MandatraValues = z.infer<typeof MandatraValueSchema>;
+interface MandatraItem {
+  value: string;
+  lineCount: number;
+}
 
 export default function Mandatra() {
   const mainRef = useRef<HTMLDivElement>(null);
-  const [values, setValues] = useState<MandatraValues>(
-    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => ""))
+  const textAreaListRef = useRef<Array<Array<HTMLTextAreaElement | null>>>(
+    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => null))
+  );
+
+  const [values, setValues] = useState<Array<Array<MandatraItem>>>(
+    Array.from({ length: 9 }, () =>
+      Array.from({ length: 9 }, () => ({
+        value: "",
+        lineCount: 0,
+      }))
+    )
   );
 
   const onImageSaveClick = async () => {
@@ -29,53 +41,82 @@ export default function Mandatra() {
   const onChange =
     (row: number, col: number) =>
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const engRegex = /^[a-z|A-Z]+$/;
+
       const isCenterArea = row === 4;
       const { value } = e.target;
-      const lineCount = value.split('\n').length;
+      const lineCount = value.split("\n").length;
 
-      if (lineCount >= 4) return;
+      if (lineCount >= 4 || engRegex.exec(value)) return;
 
       setValues((prev) => {
         /** @desc 서브 타이틀 입력 칸이라면 */
         if ((isCenterArea && col !== 4) || col === 4) {
-          prev[col][row] = value;
+          prev[col][row] = {
+            value,
+            lineCount,
+          };
         }
 
-        prev[row][col] = value;
+        prev[row][col] = {
+          value,
+          lineCount,
+        };
         return [...prev];
       });
 
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(values));
+      const stringValues = values.map((row) => row.map((col) => col.value));
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(stringValues));
     };
 
   useEffect(() => {
     const val = localStorage.getItem(LOCAL_STORAGE_KEY);
     if (!val) return;
+
     const result = MandatraValueSchema.safeParse(JSON.parse(val));
-    if (result.success) {
-      setValues(result.data);
-    }
+    if (!result.success) return;
+
+    const data = result.data.map((row) =>
+      row.map(
+        (col): MandatraItem => ({
+          value: col,
+          lineCount: col.split("\n").length,
+        })
+      )
+    );
+
+    setValues(data);
   }, []);
 
   return (
     <div className={styles["container"]}>
+      {/* <textarea className={styles["textareatemp"]} />
+      <textarea className={styles["textareatemp"]} /> */}
       <main ref={mainRef} className={styles["main-container"]}>
         {values.map((rowValue, row) => {
           const isCenterArea = row === 4;
 
           return (
-            <section
-              key={row}
-              className={styles["cell-container"]}
-            >
-              {rowValue.map((colValue, col) => {
+            <section key={row} className={styles["cell-container"]}>
+              {rowValue.map(({ value, lineCount }, col) => {
                 const isMainTitleArea = isCenterArea && col === 4;
 
                 return (
-                  <div key={`${row}-${col}`} className={styles["cell"]}>
+                  <div
+                    id={isMainTitleArea ? styles["main-title"] : ""}
+                    key={`${row}-${col}`}
+                    className={styles["cell"]}
+                  >
                     <textarea
-                      id={isMainTitleArea ? styles["main-title"] : ""}
-                      value={colValue}
+                      ref={(el) => {
+                        if (!el) return;
+                        textAreaListRef.current[row][col] = el;
+                      }}
+                      className={clsx(
+                        styles["first-line"],
+                        styles[`line-${lineCount}`]
+                      )}
+                      value={value}
                       onChange={onChange(row, col)}
                     />
                   </div>
