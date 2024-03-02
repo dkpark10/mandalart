f963f40702389e6460a5a83948ae0c2e4c603bfa
dkpark10 <dkpark10@naver.com>
2024-03-03 02:40:53 +0900

    feat: data-test 속성 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, type Plugin } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+function removeAttributes(options: Array<string>): Plugin {
+  return {
+    name: "remove-attributes",
+    enforce: "pre",
+    transform(code): string {
+      const attrs = options ?? ["data-testid"];
+      attrs.forEach((attribute) => {
+        const regex = new RegExp(
+          `(\\s(:|v-bind:)?${attribute}\\s*=\\s*(['"\`])((?:(?!\\3).)*)(\\3))|(\\s(:|v-bind:)?${attribute}(?=[\\s>]))`,
+          "gi"
+        );
+        code = code.replace(regex, "");
+      });
+      return code;
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), removeAttributes(["data-testid"])],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     port: 3000,
@@ -19,5 +35,5 @@ export default defineConfig({
   preview: {
     port: 3000,
   },
-  base: './',
-})
+  base: "./",
+});
